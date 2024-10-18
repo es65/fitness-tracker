fc105f5f979daad7489de9716d4eb8e3b8eaa264
Evan Sims <easims@gmail.com>
2024-10-18 01:37:05 -0700

    Evaluate and select model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from LearningAlgorithms import ClassificationAlgorithms
 import seaborn as sns
 import itertools
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # Plot settings
 plt.style.use("fivethirtyeight")
@@ -101,16 +101,194 @@ plt.show()
 # Grid search for best hyperparameters and model selection
 # --------------------------------------------------------------
 
+##### Look up k-Fold Cross Validation ######
+
+possible_feature_sets = [
+    feature_set_1,
+    feature_set_2,
+    feature_set_3,
+    feature_set_4,
+    selected_features
+]
+
+feature_names = [
+    'Feature Set 1',
+    'Feature Set 2',
+    'Feature Set 3',
+    'Feature Set 4',
+    'Selected Features'
+]
+
+iterations = 1
+score_df = pd.DataFrame()
+
+### Read up on Grid Search
+
+for i, f in zip(range(len(possible_feature_sets)), feature_names): # just use enumerate()
+    print("Feature set:", i)
+    selected_train_X = X_train[possible_feature_sets[i]]
+    selected_test_X = X_test[possible_feature_sets[i]]
+
+    # First run non deterministic classifiers to average their score.
+    performance_test_nn = 0
+    performance_test_rf = 0
+
+    for it in range(0, iterations):
+        print("\tTraining neural network,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.feedforward_neural_network(
+            selected_train_X,
+            y_train,
+            selected_test_X,
+            gridsearch=False,
+        )
+        performance_test_nn += accuracy_score(y_test, class_test_y)
+
+        print("\tTraining random forest,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.random_forest(
+            selected_train_X, y_train, selected_test_X, gridsearch=True
+        )
+        performance_test_rf += accuracy_score(y_test, class_test_y)
+
+    performance_test_nn = performance_test_nn / iterations
+    performance_test_rf = performance_test_rf / iterations
+
+    # And we run our deterministic classifiers:
+    print("\tTraining KNN")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.k_nearest_neighbor(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_knn = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining decision tree")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.decision_tree(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_dt = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining naive bayes")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.naive_bayes(selected_train_X, y_train, selected_test_X)
+
+    performance_test_nb = accuracy_score(y_test, class_test_y)
+
+    # Save results to dataframe
+    models = ["NN", "RF", "KNN", "DT", "NB"]
+    new_scores = pd.DataFrame(
+        {
+            "model": models,
+            "feature_set": f,
+            "accuracy": [
+                performance_test_nn,
+                performance_test_rf,
+                performance_test_knn,
+                performance_test_dt,
+                performance_test_nb,
+            ],
+        }
+    )
+    score_df = pd.concat([score_df, new_scores])
 
 # --------------------------------------------------------------
 # Create a grouped bar plot to compare the results
 # --------------------------------------------------------------
 
+# test accuracy
+score_df.sort_values(by='accuracy', ascending=False, inplace=True)
+score_df.reset_index(drop=True)
+
+plt.figure(figsize=(10,10))
+sns.barplot(x='model', y='accuracy', hue='feature_set', data=score_df)
+plt.xlabel('Model')
+plt.ylabel('Test Accuracy')
+plt.ylim(0.7, 1.0)
+plt.legend(loc="lower right")
+plt.title('Test Accuracy by Model & Feature Set')
+plt.savefig('../../reports/figures/Accuracy_vs_FeatureSet')
+plt.show()
+
+# Random forrest wins.
 
 # --------------------------------------------------------------
 # Select best model and evaluate results
 # --------------------------------------------------------------
 
+(
+    class_train_y,
+    class_test_y,
+    class_train_prob_y,
+    class_test_prob_y,
+) = learner.random_forest(
+    X_train[selected_features], y_train, X_test[selected_features], gridsearch=True
+)
+
+accuracy = accuracy_score(y_test, class_test_y)
+
+classes = list(class_test_prob_y.columns)
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+# My approach with sns.heatmap:
+plt.figure(figsize=(10,10))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+            xticklabels=classes,
+            yticklabels=classes)
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.title('Confusion Matrix')
+plt.show()
+
+# DE's code for nice confusion matrix display:
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
+
+# Not in DE code:
+
+report = classification_report(y_test, class_test_y)
+print('\nClassification Report:\n')
+print(report)
+ 
 
 # --------------------------------------------------------------
 # Select train and test data based on participant
